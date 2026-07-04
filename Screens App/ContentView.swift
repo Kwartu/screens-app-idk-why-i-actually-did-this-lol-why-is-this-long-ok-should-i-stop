a77428b663ab4f34d0908c101c0471a8d01eaff5
Kwartu :D <reworksincorporation@gmail.com>
2026-07-04 14:47:18 +0800

    This is THE WIERDEST EVER BUILT AS OF JULY 4. This is basically the foundation of this random app which I may never use ever again

--- Screens App/ContentView.swift
+++ Screens App/ContentView.swift
@@ -9,13 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color.blue
+            VStack{
+                Text("Kaartik")
+                    .bold()
+                    .font(.largeTitle)
+                Button("Say HI"){
+                    
+                }
+                .buttonStyle(.borderedProminent)
+            }
+
         }
-        .padding()
+        .ignoresSafeArea()
     }
 }
 
